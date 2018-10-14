7ee5e7b8507d1c5b252e6fb4c19a964e9e410ab3
Tomasz Pietrowski <tomasz.pietrowski@icloud.com>
2018-10-14 13:41:34 +0200

    Add kata description and tests

--- Count the Digit.playground/Contents.swift
+++ Count the Digit.playground/Contents.swift
@@ -1,5 +1,9 @@
-
+/*:
+ ### Count the Digit
+ Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer. Square all numbers k (0 <= k <= n) between 0 and n. Count the numbers of digits d used in the writing of all the k**2. Call nbDig the function taking n and d as parameters and returning this count.
+ */
 import Foundation
+import XCTest
 
 func nbDig(_ n: Int, _ d: Int) -> Int {
     return (0...n)
@@ -10,4 +14,14 @@ func nbDig(_ n: Int, _ d: Int) -> Int {
         .reduce(0, +)
 }
 
-let result = nbDig(25, 1)
+class SolutionTest: XCTestCase {
+    
+    func testExample() {
+        XCTAssertEqual(nbDig(5750, 0), 4700)
+        XCTAssertEqual(nbDig(11011, 2), 9481)
+        XCTAssertEqual(nbDig(12224, 8), 7733)
+        XCTAssertEqual(nbDig(11549, 1), 11905)
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
